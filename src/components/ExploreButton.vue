16b1a410fc1f285c66f4e293366cdd8747611301
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-10-27 18:29:23 -0400

    HTML + CSS: init explore button

---
+++ src/components/ExploreButton.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+   methods: {
+      onButtonClick() {
+         // do nothing for now
+         console.log('Explore button clicked')
+      }
+   }
+}
+</script>
+
+<template>
+   <button
+      class="explore-button"
+      @click="onButtonClick"
+   >Explore</button>
+</template>
+
+<style lang="scss">
+.explore-button {
+   height: var(--explore-button-size);
+   width: var(--explore-button-size);
+   font-size: var(--explore-button-font-size);
+
+   color: var(--explore-button-color);
+   background-color: var(--explore-button-background-color);
+
+   border-radius: 100%;
+   border: none;
+
+   text-transform: uppercase;
+}
+</style>
