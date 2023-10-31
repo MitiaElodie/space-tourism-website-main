c1bf7a73a55eef7e0924dde8d07d836421f7c258
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-10-31 19:48:04 -0400

    CSS + HTML: add paddings to main tag

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,36 @@
 </script>
 
 <template>
-   <div class="app">
-      <header>
-      </header>
-      <main>
-         <RouterView />
-      </main>
-   </div>
+   <header>
+   </header>
+   <main class="app__main">
+      <RouterView />
+   </main>
 </template>
 
 <style lang="scss">
+@use '@/assets/base.scss';
+
+.app {
+   --main-horizontal-padding: 24px;
+   --main-vertical-padding: 40px;
+
+   &__main {
+      padding: var(--main-horizontal-padding) var(--main-vertical-padding);
+   }
+}
+
+@media (min-width: base.$tablet-breakpoint) {
+      .app {
+         --main-horizontal-padding: 165px;
+         --main-vertical-padding: 76px;
+      }
+   }
+
+   @media (min-width: base.$laptop-breakpoint) {
+      .app {
+         --main-horizontal-padding: 165px;
+         --main-vertical-padding: 76px;
+      }
+   }
 </style>
