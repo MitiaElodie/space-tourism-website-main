4f696387dd80399eb42f88965ea84ebc93ebbdb2
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-12-04 20:38:56 -0500

    JS + HTML + CSS: create the textTabs component

---
+++ src/components/TextTabs.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+   props: {
+      tabList: {
+         type: Array,
+         required: true,
+      },
+
+      initiallySelected: {
+         type: String,
+         default: null
+      }
+   },
+
+   emits: ['click'],
+
+   data() {
+      return {
+         selected: null,
+      }
+   },
+
+   created() {
+      this.selected = this.initiallySelected
+   },
+
+   methods: {
+      onButtonClick(tabId) {
+         this.$emit('click', tabId)
+         this.selected = tabId
+      }
+   }
+}
+</script>
+
+<template>
+   <ul class="text-tabs__container">
+      <li
+         class="text-tabs__item"
+         v-for="tab in tabList"
+         :key="tab.id"
+      >
+         <button
+            class="text-tabs__button"
+            :class="{'text-tabs__button--selected': selected === tab.id}"
+            @click="onButtonClick(tab.id)"
+         >{{ tab.name }}</button>
+      </li>
+   </ul>
+</template>
+
+<style lang="scss">
+.text-tabs {
+   &__container {
+      display: flex;
+   }
+
+   &__item {
+      list-style: none;
+   }
+
+   &__button {
+      background: none;
+      color: var(--color-text);
+      border: none;
+      text-transform: uppercase;
+      padding: 10px;
+
+      &:hover {
+         border-bottom: 2px solid var(--tab-border-bottom-hover-color);
+      }
+
+      &--selected,
+      &--selected:hover {
+         border-bottom: 2px solid var(--tab-border-bottom-active-color);
+      }
+   }
+}
+</style>
