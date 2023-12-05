2d926cbd0c976297dbf2721971adbd1aeb9d0f7e
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-12-04 20:39:22 -0500

    JS + HTML + CSS: add some information to DestinationView

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,8 +1,53 @@
 <script>
 import PageBase from '@/components/PageBase.vue';
+import TextTabs from '@/components/TextTabs.vue';
+import DestinationDetails from '@/components/DestinationDetails.vue';
 
+const destinationList = [
+        {
+          id: 'moon',
+          name: 'Moon',
+          description: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+          distance: '384,400',
+          travelTime: '3 days',
+        },
+        {
+          id: 'mars',
+          name: 'Mars',
+          description: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+          distance: '225 mil.',
+          travelTime: '9 months',
+        },
+        {
+          id: 'europa',
+          name: 'Europa',
+          description: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+          distance: '628 mil.',
+          travelTime: '3 years',
+        },
+        {
+          id: 'titan',
+          name: 'Titan',
+          description: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+          distance: '1.6 bil.',
+          travelTime: '7 years',
+        },
+      ]
 export default {
-  components: {PageBase},
+  components: { PageBase, TextTabs, DestinationDetails },
+
+  data() {
+    return {
+      selected: destinationList[0],
+      destinationList,
+    }
+  },
+
+  methods: {
+    onDestinationClick(tabId) {
+      this.selected = destinationList.find((item) => item.id === tabId)
+    }
+  }
 }
 </script>
 <template>
@@ -10,6 +55,23 @@ export default {
     order="01"
     title="Pick your destination"
   >
-    a
+    <div
+      class="destination-view__container"
+    >
+      <div class="destination-view__image-container">
+        <img />
+      </div>
+      <div class="destination-view__information-container">
+        <TextTabs
+          class="destination-view__tabs"
+          :tab-list="destinationList"
+          :initially-selected="selected.id"
+          @click="onDestinationClick"
+        />
+        <DestinationDetails
+          :destination="selected"
+        />
+      </div>
+    </div>
   </PageBase>
 </template>
